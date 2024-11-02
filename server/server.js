bb8b6f5b2eb33028a3ab081b27a9d7085ac97253
YeshaswiniVasudevaMurthy <Y.VasudevaMurthy001@umb.edu>
2024-11-02 16:56:08 -0400

    changed to aws database for testing

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
@@ -22,17 +22,10 @@ app.use("/api/questions", questionRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/feedback", feedbackRoutes);
 
-// Connect to MySQL and start the server
-db.connect((err) => {
-  if (err) {
-    console.error("Unable to connect to the database:", err);
-    process.exit(1); // Exit the app if the database connection fails
-  } else {
-    console.log("Database connected");
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  }
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 app.get("/", (req, res) => {
