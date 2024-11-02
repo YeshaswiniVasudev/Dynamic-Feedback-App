de644326f65d73a874fa5518ad720d0b4cca360e
YeshaswiniVasudevaMurthy <Y.VasudevaMurthy001@umb.edu>
2024-11-02 11:57:09 -0400

    abd

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://feedback-dp70270z4-yeshaswini-vasudeva-murthys-projects.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
