1a4625819b54f3a59b94d759df4c41c80c6381d1
YeshaswiniVasudevaMurthy <Y.VasudevaMurthy001@umb.edu>
2024-11-02 17:39:02 -0400

    added message in error

--- server/db.js
+++ server/db.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error('Database connection failed:', err.stack);
+    console.error('Database connection failed:', err.message, err.stack);
     return;
   }
   console.log('Connected to MySQL database');
